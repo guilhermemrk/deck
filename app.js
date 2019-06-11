27dd2f6325221538579625aa74bff71209c61f5b
Madaoyasha <guilhermemr_kk@live.com>
2019-06-11 17:51:19 -0400

    v0.1

---
+++ app.js
@@ -0,0 +1,24 @@
+const readline = require('readline-sync')
+var i = 1
+
+var numDeck = readline.question("How many decks?")
+var cards = numDeck*52
+console.log(`Decks: ${numDeck}\nCards: ${cards} (52 per deck)`);
+
+while (i = 1) {
+  var option = readline.question("Select an option:\n\n1- Remove a card\n2- Calculate probability")
+  // var iOption = Convert.ToInt32(option);
+
+  if (option = '1'){
+    var justKidding = readline.question('What card would you like to remove?')
+    cards--
+    console.log(`${justKidding} got removed from the deck, ${cards} remaining`)
+  }
+  else if (option = '2') {
+    var selected = readline.question('What card would you like to pull?')
+    console.log(`You selected ${selected}!`)
+    console.log(`The probability of pulling ${selected} is ${1/cards}`);
+  } else {
+
+  }
+}
